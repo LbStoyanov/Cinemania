230ab6a122f7a7c04e7a9ef3cf1736223f41bfcc
LbStoyanov <lbstoyanov1@gmail.com>
2023-08-22 21:52:56 +0300

    Now Playing datasource implementation

--- cinemania/lib/domain/datasources/movies_datasource.dart
+++ cinemania/lib/domain/datasources/movies_datasource.dart
@@ -1,5 +1,5 @@
 import 'package:cinemania/domain/entities/movie.dart';
 
-abstract class MovieDatasource {
+abstract class MoviesDatasource {
   Future<List<Movie>> getNowPlaying({int page = 1});
 }
