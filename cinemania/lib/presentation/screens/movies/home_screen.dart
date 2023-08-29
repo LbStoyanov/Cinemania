44a62f103daef78ae8b0b8b5acbbcf9a0fb45b0f
LbStoyanov <lbstoyanov1@gmail.com>
2023-08-29 19:32:14 +0300

    Added http getNowPlaying from the movie db

--- cinemania/lib/presentation/screens/movies/home_screen.dart
+++ cinemania/lib/presentation/screens/movies/home_screen.dart
@@ -33,6 +33,16 @@ class _HomeViewState extends ConsumerState<_HomeView> {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
+    return ListView.builder(
+      itemCount: nowPlayingMovies.length,
+      itemBuilder: (context, index) {
+        final movie = nowPlayingMovies[index];
+
+        return ListTile(
+          title: Text(movie.title),
+        );
+      },
+    );
   }
 }
