b13e6a50c7d00fb1b8b86adf253ac9733d0be383
LbStoyanov <lbstoyanov1@gmail.com>
2023-10-11 20:48:44 +0200

    Creating getMovieDetailsById implementation

--- cinemania/lib/infrastructure/datasources/moviedb_datasource.dart
+++ cinemania/lib/infrastructure/datasources/moviedb_datasource.dart
@@ -1,4 +1,5 @@
 import 'package:cinemania/infrastructure/mappers/movie_mapper.dart';
+import 'package:cinemania/infrastructure/models/moviedb/movie_details.dart';
 import 'package:cinemania/infrastructure/models/moviedb/moviedb_response.dart';
 import 'package:dio/dio.dart';
 import 'package:cinemania/config/constants/environment.dart';
@@ -58,5 +59,20 @@ class MoviedbDatasource extends MoviesDatasource {
     });
 
     return _jsonToMoviesList(response.data);
+  }
+
+  @override
+  Future<Movie> getMovieById(String id) async {
+    final response = await dio.get('/movie/$id');
+
+    if (response.statusCode != 200) {
+      throw Exception('Movie with id $id not found');
+    }
+
+    final movieDetails = MovieDetails.fromJson(response.data);
+
+    final Movie movie = MovieMapper.movieDetailsToEntity(movieDetails);
+
+    return movie;
   }
 }
