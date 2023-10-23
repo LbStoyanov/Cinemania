bb3ce581711e218d3b92d41b6d513d13be77f6ff
LbStoyanov <lbstoyanov1@gmail.com>
2023-10-23 18:58:09 +0200

    Add movie details provider

---
+++ cinemania/lib/presentation/providers/movies/movie_details_provider.dart
@@ -0,0 +1,30 @@
+import 'package:cinemania/domain/entities/movie.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'movies_repository_provider.dart';
+
+final movieDetailsProvider = StateNotifierProvider(
+  (ref) {
+    final movieRepo = ref.watch(movieRepositoryProvider);
+
+    return MovieMapNotifier(getMovie: movieRepo.getMovieById);
+  },
+);
+
+typedef GetMovieCallback = Future<Movie> Function(String movieId);
+
+class MovieMapNotifier extends StateNotifier<Map<String, Movie>> {
+  MovieMapNotifier({required this.getMovie}) : super({});
+
+  final GetMovieCallback getMovie;
+
+  Future<void> loadMovie(String movieId) async {
+    if (state[movieId] != null) {
+      return;
+    }
+
+    final movie = await getMovie(movieId);
+
+    state = {...state, movieId: movie};
+  }
+}
