fa690cfc71ce109ff1b7c4ca91b3ba637cbb8488
LbStoyanov <lbstoyanov1@gmail.com>
2024-01-17 06:37:29 +0200

    Adding Movie Screen and Actors Details

--- cinemania/lib/presentation/screens/movies/movie_screen.dart
+++ cinemania/lib/presentation/screens/movies/movie_screen.dart
@@ -124,9 +124,6 @@ class _MovieDetails extends StatelessWidget {
           ),
         ),
         _ActorsByMovie(movieId: movie.id.toString()),
-        const SizedBox(
-          height: 80,
-        )
       ],
     );
   }
@@ -141,9 +138,75 @@ class _ActorsByMovie extends ConsumerWidget {
   Widget build(BuildContext context, ref) {
     final actorsByMovie = ref.watch(actorsByMovieProvider);
 
-    
+    if (actorsByMovie[movieId] == null) {
+      return const CircularProgressIndicator(
+        strokeWidth: 2,
+      );
+    }
 
-    return const Placeholder();
+    final actors = actorsByMovie[movieId]!;
+
+    return SizedBox(
+      height: 300,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: actors.length,
+        itemBuilder: (context, index) {
+          final actor = actors[index];
+
+          return Container(
+            padding: const EdgeInsets.all(8.0),
+            width: 135,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                //Actor Photo
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.network(
+                    actor.profilePath,
+                    height: 180,
+                    width: 120,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                //Actor Name
+                const SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  actor.name,
+                  maxLines: 2,
+                ),
+                Text(
+                  actor.character ?? '',
+                  maxLines: 2,
+                  style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 51, 159, 248)),
+                ),
+                /*  ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Container(
+                    color: const Color.fromARGB(255, 16, 240, 23),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        actor.character ?? '',
+                        maxLines: 2,
+                        style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            overflow: TextOverflow.ellipsis),
+                      ),
+                    ),
+                  ),
+                ), */
+              ],
+            ),
+          );
+        },
+      ),
+    );
   }
 }
 
@@ -158,12 +221,12 @@ class _CustomSliverAppBar extends StatelessWidget {
 
     return SliverAppBar(
       backgroundColor: Colors.lightBlue[400],
-      expandedHeight: size.height * 0.6,
+      expandedHeight: size.height * 0.7,
       foregroundColor: Colors.white,
       flexibleSpace: FlexibleSpaceBar(
         titlePadding: const EdgeInsets.symmetric(
-          horizontal: 10,
-          vertical: 5,
+          horizontal: 8,
+          vertical: 2,
         ),
         // title: Text(
         //   movie.title,
