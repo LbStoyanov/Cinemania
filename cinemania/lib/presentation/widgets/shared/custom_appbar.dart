44900e05b217cb633352307c4cf5b915b87b212c
LbStoyanov <lbstoyanov1@gmail.com>
2024-01-22 05:49:29 +0200

    Adding Search bar logic for search movies

--- cinemania/lib/presentation/widgets/shared/custom_appbar.dart
+++ cinemania/lib/presentation/widgets/shared/custom_appbar.dart
@@ -1,11 +1,13 @@
 import 'package:cinemania/presentation/delegates/search_movie_delegate.dart';
+import 'package:cinemania/presentation/providers/movies/movies_repository_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class CustomAppbar extends StatelessWidget {
+class CustomAppbar extends ConsumerWidget {
   const CustomAppbar({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final colors = Theme.of(context).colorScheme;
 
     return SafeArea(
@@ -29,7 +31,11 @@ class CustomAppbar extends StatelessWidget {
               const Spacer(),
               IconButton(
                 onPressed: () {
-                  showSearch(context: context, delegate: SearchMovieDelegate());
+                  final movieRepository = ref.watch(movieRepositoryProvider);
+                  showSearch(
+                      context: context,
+                      delegate: SearchMovieDelegate(
+                          searchMovies: movieRepository.searchMovies));
                 },
                 icon: const Icon(Icons.search),
               ),
